5390b275288e42f6cbf111f2daaab81b25ea24e9
Javad HAMIDI <javad.hamidi@CURRIC.GIHS.SA.EDU.AU>
2017-09-05 09:55:11 +0930

    added assets

---
+++ misc.py
@@ -0,0 +1,21 @@
+num_mines = 5
+
+cell_num = 1
+
+# create grid
+num_rows = 5
+num_columns = 5
+
+
+class Cell:
+    def __init__(self, mine, num, clicked):
+        self.mine = False
+        self.num = 0
+        self.clicked = False
+
+for row in range(num_rows):
+    for cell in range(num_columns):
+        (cell + str(cell_num)) = Cell.(True, 0, False)
+        print(str(cell + str(cell_num)))
+        cell_num += 1
+
